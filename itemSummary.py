b34ea76acb078c1d3530d9f70c9a1523217be8dc
Adam Abouelela <159411873+adam-abo@users.noreply.github.com>
2026-09-13 01:40:26 -0700

    Replace hardcoded paths with project directory placeholder
    Updated file paths in functions to use a placeholder for the project directory.

--- itemSummary.py
+++ itemSummary.py
@@ -1,12 +1,12 @@
 import os
 
-def clearFolder(path='/Users/adamabouelela/Desktop/Memory-Match-Macro/Boards'):
+def clearFolder(path='/XXX/Memory-Match-Macro/Boards'): # Replace XXX with project directory
     for file in os.listdir(path):
         os.remove(os.path.join(path, file)) 
 
 def identify(items, collected):
     known_items = {}
-    with open('/Users/adamabouelela/Desktop/Memory-Match-Macro/RGB Values.txt', 'r') as file:
+    with open('/XXX/Memory-Match-Macro/RGB Values.txt', 'r') as file:  # Replace XXX with project directory
         # Read each line in the file
         for line in file:
             # Split the line by colon ':' to separate key and value
@@ -28,7 +28,7 @@ def identify(items, collected):
     return collected
 
 def writeSummary(items, games = {}):
-    with open('/Users/adamabouelela/Desktop/Memory-Match-Macro/Session Summary.rtf', 'w') as f:
+    with open('/XXX/Memory-Match-Macro/Session Summary.rtf', 'w') as f:  # Replace XXX with project directory
         f.write('{\\rtf1\\ansi\\ansicpg1252\\cocoartf2759\n\\cocoatextscaling0\\cocoaplatform0{\\fonttbl\\f0\\fswiss\\fcharset0 Helvetica-Bold;\\f1\\fswiss\\fcharset0 Helvetica;}\n{\\colortbl;\\red255\\green255\\blue255;}\n{\\*\\expandedcolortbl;;}\n{\\*\\listtable{\\list\\listtemplateid1\\listhybrid{\\listlevel\\levelnfc23\\levelnfcn23\\leveljc0\\leveljcn0\\levelfollow0\\levelstartat1\\levelspace360\\levelindent0{\\*\\levelmarker \\{hyphen\\}}{\\leveltext\\leveltemplateid1\\\'01\\uc0\\u8259 ;}{\\levelnumbers;}\\fi-360\\li720\\lin720 }{\\listlevel\\levelnfc23\\levelnfcn23\\leveljc0\\leveljcn0\\levelfollow0\\levelstartat1\\levelspace360\\levelindent0{\\*\\levelmarker \\{hyphen\\}}{\\leveltext\\leveltemplateid2\\\'01\\uc0\\u8259 ;}{\\levelnumbers;}\\fi-360\\li1440\\lin1440 }{\\listname ;}\\listid1}\n{\\list\\listtemplateid2\\listhybrid{\\listlevel\\levelnfc23\\levelnfcn23\\leveljc0\\leveljcn0\\levelfollow0\\levelstartat1\\levelspace360\\levelindent0{\\*\\levelmarker \\{hyphen\\}}{\\leveltext\\leveltemplateid101\\\'01\\uc0\\u8259 ;}{\\levelnumbers;}\\fi-360\\li720\\lin720 }{\\listlevel\\levelnfc23\\levelnfcn23\\leveljc0\\leveljcn0\\levelfollow0\\levelstartat1\\levelspace360\\levelindent0{\\*\\levelmarker \\{hyphen\\}}{\\leveltext\\leveltemplateid102\\\'01\\uc0\\u8259 ;}{\\levelnumbers;}\\fi-360\\li1440\\lin1440 }{\\listname ;}\\listid2}}\n{\\*\\listoverridetable{\\listoverride\\listid1\\listoverridecount0\\ls1}{\\listoverride\\listid2\\listoverridecount0\\ls2}}\n\\margl1440\\margr1440\\vieww17500\\viewh12440\\viewkind0\n\\pard\\tx566\\tx1133\\tx1700\\tx2267\\tx2834\\tx3401\\tx3968\\tx4535\\tx5102\\tx5669\\tx6236\\tx6803\\sl360\\slmult1\\pardirnatural\\partightenfactor0\n\n\\f0\\b\\fs36 \\cf0 \\ul \\ulc0 ')
         f.write('Macro Session Summary:') # Header
         f.write('\n\\f1\\b0\\fs30 \\ulnone \\\n\\pard\\tx566\\tx1133\\tx1700\\tx2267\\tx2834\\tx3401\\tx3968\\tx4535\\tx5102\\tx5669\\tx6236\\tx6803\\sl360\\slmult1\\pardirnatural\\partightenfactor0\n\n\\fs32 \\cf0 	\\ul ')
